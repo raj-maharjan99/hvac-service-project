b6aa6c79cddc1cc587a10b36650feab7fd829dd5
rajkumar88 <rjuallone@gmail.com>
2025-06-22 17:20:42 -0500

    use strait font from google

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,28 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, JetBrains_Mono, Strait } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+// const jetBrains = JetBrains_Mono({
+//   subsets: ["latin"],
+//   weight: ["400", "700"], // optional weights
+//   display: "swap",
+// });
+const straitFont = Strait({
   subsets: ["latin"],
+  weight: ["400"], // optional weights
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -27,7 +38,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={straitFont.className}
       >
         <Navbar />
         {children}
